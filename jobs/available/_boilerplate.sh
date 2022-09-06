28aa35f49fd0d29e15b97342b588bfaab189e98e
Achim Sperling <achim.sperling@gmail.com>
2022-09-06 12:38:02 +0200

    ✨ Add job template to backup docker containers

--- jobs/available/_boilerplate.sh
+++ jobs/available/_boilerplate.sh
@@ -7,10 +7,12 @@ if [ -z "${SCRIPTDIR}" ]; then
     exit 1
 fi
 
+source "${SCRIPTDIR}/lib/utils.sh"
+
 JOBNAME="My Job"
 info "${JOBNAME}" "Running Job"
 
-# Put any files you like to backup in ${BACKUP_TARGETDIR} in order to get the rsynced.
+# Put any files you like to backup in ${BACKUP_TARGETDIR} in order to get it rsynced.
 # Use `[info|error|print] "${JOBNAME}" "Some messsage"` to log/echo to stdout. 
 # Just exit with any error code if necessary and all files in ${BACKUP_TARGETDIR}
 # will get removed automatically.
